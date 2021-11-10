8e0edd28e3fd580a9ca3440ca4ecb275a97c83d8
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-10 12:28:56 +0530

    UC1TicTacToe

--- TicTocToeBoard/TicTacToeGame.cs
+++ TicTocToeBoard/TicTacToeGame.cs
@@ -10,9 +10,9 @@ namespace TicTocToeBoard
     {
         public void UC1TikTacToe()
         {
-            char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-            for(int i=1;i<10;i++)
+            char[] board = new char[10] { '0','1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        
+            for(int i=1;i<board.Length;i++)
             {
                 Console.WriteLine(board[i]);
             }
