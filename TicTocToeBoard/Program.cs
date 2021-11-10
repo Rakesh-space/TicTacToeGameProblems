5ad42010d43d9057c45815d906c638b38ee131f3
Rakesh-space <rakeshsmusale03@gmail.com>
2021-11-10 12:02:07 +0530

    UC1 Tic Tac Toe Game

---
+++ TicTocToeBoard/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTocToeBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to Tic Tac Toe Game...");
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.UC1TikTacToe();
+        }
+    }
+}
---
+++ TicTocToeBoard/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTocToeBoard
+{
+    class TicTacToeGame
+    {
+        public void UC1TikTacToe()
+        {
+            char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            for(int i=1;i<10;i++)
+            {
+                Console.WriteLine(board[i]);
+            }
+        }
+    }
+}
